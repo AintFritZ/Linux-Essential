94f221b6a8b77dbb3c9fe57a009e96c5635f483a
Eti <114860898+AintFritZ@users.noreply.github.com>
2025-02-05 01:42:40 +0000

    Activity1

---
+++ Activity1.sh
@@ -0,0 +1,15 @@
+USER_NAME=""
+AGE=0
+PROGRAM=""
+
+
+echo "Enter your name : "
+read USER_NAME
+
+echo "Enter your Age : "
+read AGE
+
+echo "Enter your Program : "
+read PROGRAM
+
+echo "Hello $USER_NAME! Your age is $AGE, and your program is $PROGRAM"
---
+++ sample.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# A simple variable example
+
+USER_NAME=""
+AGE=0
+PROGRAM=""
+
+
+echo "Enter your name : "
+read USER_NAME
+
+echo "Enter your Age : "
+read AGE
+
+echo "Enter your Program : "
+read PROGRAM
+
+
+echo "Hello $USER_NAME! Your age is $AGE, and your program is $PROGRAM"
